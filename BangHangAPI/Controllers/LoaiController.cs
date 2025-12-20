1a2796b0990ce90b6df0c821998582f024bea9ac
LitteHevil <hungninklon.game@gmail.com>
2025-12-21 00:30:33 +0700

    Them chuc nang xoa Loai

--- BangHangAPI/Controllers/LoaiController.cs
+++ BangHangAPI/Controllers/LoaiController.cs
@@ -32,7 +32,47 @@ namespace BangHangAPI.Controllers
             _context.SaveChanges();
             return StatusCode(201, loai);
         
+        
+      }
+        // PUT: api/Loai/1 (Số 1 là id cần sửa)
+        [HttpPut("{id}")] // cần id thì mới update đc 
+        public IActionResult UpdateLoaiById(int id, Loai loaiEdit)
+        {
+            // Tìm xem có cái loại đó trong kho không?
+            var loaiCanSua = _context.loais.SingleOrDefault(loai => loai.MaLoai == id);
+
+            // Nếu tìm không thấy -> Báo lỗi 404 Not Found
+            if (loaiCanSua == null)
+            {
+                return NotFound();
+            }
+
+            // Bước 2: Nếu thấy -> Sửa thông tin
+            loaiCanSua.TenLoai = loaiEdit.TenLoai;
+
+            // Bước 3: Lưu thay đổi
+            _context.SaveChanges();
+
+            return Ok(loaiCanSua); // Trả về cục đã sửa xong
         }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteLoaiById(int id) {
+            // tìm id có có tồn tại 
+
+            var LoaiCanXoa = _context.loais.SingleOrDefault(xoa => xoa.MaLoai == id);
+            if (LoaiCanXoa == null)
+            {
+                return NotFound();
+            }
+
+            //thuc hien xoa
+            _context.loais.Remove(LoaiCanXoa);
+            //save
+            _context.SaveChanges();
+
+            return StatusCode(200);
+        }
+
 
     }
 }
