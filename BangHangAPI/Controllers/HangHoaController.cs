9b8695d623fe8f9912297bfbf2710d9e454bf860
LitteHevil <hungninklon.game@gmail.com>
2025-12-26 01:07:24 +0700

    cập nhật thêm hàng hoá

--- BangHangAPI/Controllers/HangHoaController.cs
+++ BangHangAPI/Controllers/HangHoaController.cs
@@ -19,6 +19,27 @@ namespace BangHangAPI.Controllers
         return Ok(dsHangHoa);
         }
 
+        [HttpGet("{id}")]
+        public IActionResult GetbyId(int id) {
+            var hanghoa = _context.hanghoa.FirstOrDefault(hh => hh.MaHangHoa == id);
+
+            if (hanghoa == null) { 
+                return NotFound();
+            }
+            // nếu thấy thì trả dữ liệu về 
+            return Ok(hanghoa);
+        }
+
+        //them mới sản phầm
+        [HttpPost]
+        public IActionResult Create(HangHoa hanghoa) { 
+             // thêm dữ liệu vào database
+            _context.hanghoa.Add(hanghoa);
+            // lưu thay đổi 
+            _context.SaveChanges();
+
+            return StatusCode(201, hanghoa);
+               }
     
 
 
